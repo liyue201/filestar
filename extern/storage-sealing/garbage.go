8c7898095b580e11134a3db8f4c8e260ede5e0ee
liyue201 <liyue201@126.com>
2020-11-04 10:05:56 +0800

    添加误删的文件

---
+++ extern/storage-sealing/garbage.go
@@ -0,0 +1,120 @@
+package sealing
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"golang.org/x/xerrors"
+
+	"github.com/filecoin-project/go-state-types/abi"
+)
+
+func (m *Sealing) pledgeSector(ctx context.Context, sectorID abi.SectorID, existingPieceSizes []abi.UnpaddedPieceSize, sizes ...abi.UnpaddedPieceSize) ([]abi.PieceInfo, error) {
+	if len(sizes) == 0 {
+		return nil, nil
+	}
+
+	log.Infof("Pledge %d, contains %+v", sectorID, existingPieceSizes)
+
+	out := make([]abi.PieceInfo, len(sizes))
+	for i, size := range sizes {
+		ppi, err := m.sealer.AddPiece(ctx, sectorID, existingPieceSizes, size, NewNullReader(size))
+		if err != nil {
+			return nil, xerrors.Errorf("add piece: %w", err)
+		}
+
+		existingPieceSizes = append(existingPieceSizes, size)
+
+		out[i] = ppi
+	}
+
+	return out, nil
+}
+
+func (m *Sealing) PledgeSector() error {
+	cfg, err := m.getConfig()
+	if err != nil {
+		return xerrors.Errorf("getting config: %w", err)
+	}
+
+	if cfg.MaxSealingSectors > 0 {
+		if m.stats.curSealing() > cfg.MaxSealingSectors {
+			return xerrors.Errorf("too many sectors sealing (curSealing: %d, max: %d)", m.stats.curSealing(), cfg.MaxSealingSectors)
+		}
+	}
+	if !m.sealer.CanAddPiece() {
+		return errors.New("too many sectors sealing, out off task limit")
+	}
+
+	go func() {
+		ctx := context.TODO() // we can't use the context from command which invokes
+		// this, as we run everything here async, and it's cancelled when the
+		// command exits
+
+		size := abi.PaddedPieceSize(m.sealer.SectorSize()).Unpadded()
+
+		sid, err := m.sc.Next()
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+		err = m.sealer.NewSector(ctx, m.minerSector(sid))
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+
+		pieces, err := m.pledgeSector(ctx, m.minerSector(sid), []abi.UnpaddedPieceSize{}, size)
+		if err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+
+		ps := make([]Piece, len(pieces))
+		for idx := range ps {
+			ps[idx] = Piece{
+				Piece:    pieces[idx],
+				DealInfo: nil,
+			}
+		}
+
+		if err := m.newSectorCC(sid, ps); err != nil {
+			log.Errorf("%+v", err)
+			return
+		}
+	}()
+	return nil
+}
+
+func (m *Sealing) RunPledgeSectors(ctx context.Context) error {
+	timer := time.NewTimer(time.Minute)
+	for {
+		select {
+		case <-timer.C:
+			cfg, err := m.getConfig()
+			if err != nil || !cfg.AutoPledgeSector {
+				log.Infof("%v, conf: %+v", err, cfg)
+				break
+			}
+			for {
+				if err := m.PledgeSector(); err != nil {
+					break
+				}
+				wait := time.After(time.Second * 5)
+				select {
+				case <-wait:
+				case <-ctx.Done():
+					return nil
+				}
+			}
+			if cfg.AutoPledgeSectorInterval == 0 {
+				cfg.AutoPledgeSectorInterval = 10
+			}
+			timer.Reset(time.Minute * time.Duration(cfg.AutoPledgeSectorInterval))
+		case <-ctx.Done():
+			return nil
+		}
+	}
+	return nil
+}
